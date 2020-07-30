16a69f5f451d5e914ea631ad546958ac4618c2a4
Steve Ballantine <Stephen@51degrees.com>
2020-07-30 17:00:10 +0100

    REF: Updated dependencies to v4.1.10. EXAMPLE: Modified web integration example to take advantage of the new, simpler approach.

--- Examples/Cloud/AspNetCore3.1/Controllers/HomeController.cs
+++ Examples/Cloud/AspNetCore3.1/Controllers/HomeController.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using FiftyOne.GeoLocation.Core.Data;
-using FiftyOne.Pipeline.JavaScriptBuilder.Data;
-using FiftyOne.Pipeline.JsonBuilder.Data;
 using FiftyOne.Pipeline.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,23 +22,6 @@ namespace AspNetCore31.Controllers
         {
             var data = _flow.GetFlowData().Get<IGeoData>();
             return View(data);
-        }
-
-        /// <summary>
-        /// Endpoint that will be called by the client-side code to get 
-        /// the JSON formatted results from the Pipeline.
-        /// </summary>
-        /// <returns>
-        /// The output of the Pipeline for the current request formatted 
-        /// as JSON.
-        /// </returns>
-        public IActionResult Json()
-        {
-            return new ContentResult()
-            {
-                Content = _flow.GetFlowData().Get<IJsonBuilderElementData>().Json,
-                ContentType = "application/json"
-            };
         }
     }
 }
