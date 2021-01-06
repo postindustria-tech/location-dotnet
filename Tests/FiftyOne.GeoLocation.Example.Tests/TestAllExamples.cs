9d8de7e70c84c17fe82aab97e58fe2952fb579c8
Joseph Dix <joseph@51degrees.onmicrosoft.com>
2021-01-06 16:57:56 +0000

    FEAT: Updates for release 4.2
    REF: Update references to pipeline and device detection packages.
    EXAMPLE: Refactor examples so that they are unit testable.
    TEST: Add tests for examples.
    TEST: Add tests for GeoEngineHelper.
    DOC: Update doxy file.

---
+++ Tests/FiftyOne.GeoLocation.Example.Tests/TestAllExamples.cs
@@ -0,0 +1,81 @@
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2020 51 Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY.
+ *
+ * This Original Work is licensed under the European Union Public Licence (EUPL) 
+ * v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ * 
+ * If using the Work as, or as part of, a network application, by 
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading, 
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FiftyOne.GeoLocation.Example.Tests
+{
+    /// <summary>
+    /// This test class ensures that all examples execute successfully.
+    /// </summary>
+    /// <remarks>
+    /// Note that these test do not generally ensure the correctness 
+    /// of the example, only that the example will run without 
+    /// crashing or throwing any unhandled exceptions.
+    /// </remarks>
+    [TestClass]
+    public class TestAllExamples
+    {
+        private string ResourceKey;
+
+        /// <summary>
+        /// Init method - specify Resource Key to run examples here or 
+        /// set a Resource Key in an environment variable called 'ResourceKey'.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            var resourceKey = Environment.GetEnvironmentVariable("LOCATIONRESOURCEKEY");
+            ResourceKey = string.IsNullOrWhiteSpace(resourceKey) == false ? 
+                resourceKey : "!!YOUR_RESOURCE_KEY!!";
+
+            if (string.IsNullOrWhiteSpace(ResourceKey) == true ||
+                ResourceKey.StartsWith("!!") == true)
+            {
+                Assert.Fail("ResourceKey must be specified in the Init method" +
+                    " or as an Environment variable");
+            }
+        }
+
+
+        /// <summary>
+        /// Test that the GettingStarted example runs successfully.
+        /// </summary>
+        [TestMethod]
+        public void GettingStarted()
+        {
+            var example = new GettingStarted.Program.Example();
+            example.Run(ResourceKey);
+        }
+
+        /// <summary>
+        /// Test the the CombiningServices example runs successfully.
+        /// </summary>
+        [TestMethod]
+        public void CombiningServices()
+        {
+            var example = new CombiningServices.Program.Example();
+            example.Run(ResourceKey);
+        }
+    }
+}
---
+++ Tests/FiftyOne.GeoLocation.Tests/Constants.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiftyOne.GeoLocation.Tests
+{
+    /// <summary>
+    /// GeoLocation test constants
+    /// </summary>
+    internal class Constants
+    {
+        /// <summary>
+        /// Sample response returned by a geo location api server. 
+        /// In this case, nominatim.
+        /// </summary>
+        internal const string JSON_RESPONSE = @"
+{
+    'place_id': 85642204,
+    'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
+    'osm_type': 'way',
+    'osm_id': 96177745,
+    'lat': '51.4578261',
+    'lon': '-0.975922996290084',
+    'display_name': 'Spaces, 9, Greyfriars Road, Coley, Reading, South East, England, RG1 1NU, United Kingdom',
+    'address': {
+        'place': 'Spaces',
+        'house_number': '9',
+        'road': 'Greyfriars Road',
+        'suburb': 'Coley',
+        'town': 'Reading',
+        'county': 'Reading',
+        'state_district': 'South East',
+        'state': 'England',
+        'postcode': 'RG1 1NU',
+        'country': 'United Kingdom',
+        'country_code': 'gb'
+    },
+    'boundingbox': ['51.4575761', '51.4580634', '-0.9764213', '-0.9757286']
+}";
+
+        internal const string TEST_LAT = "51.4578261";
+        internal const string TEST_LON = "-0.975922996290084";
+    }
+}
